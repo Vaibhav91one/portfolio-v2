c85e9d7c874018af24dceab762630daf3f3af3a4
Vaibhav91one <vtking03@gmail.com>
2025-04-05 02:59:11 +0530

    Robot fix

--- components/3D/Robot.tsx
+++ components/3D/Robot.tsx
@@ -1,17 +1,12 @@
-// app/page.tsx (or wherever you’re using it)
+// app/robot/page.tsx or app/page.tsx
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const Spline = dynamic(() => import('@splinetool/react-spline').then(mod => mod.default), {
-  ssr: false,
-});
+import Spline from '@splinetool/react-spline';
 
 export default function Robot() {
   return (
-    <main className='w-fit h-full'>
-      <Spline 
-      scene="https://prod.spline.design/M4S11dvJnr65ej9S/scene.splinecode" />
+    <main className="w-fit h-full">
+      <Spline scene="https://prod.spline.design/M4S11dvJnr65ej9S/scene.splinecode" />
     </main>
   );
 }
