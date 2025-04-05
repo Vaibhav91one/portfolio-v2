9f9cf2f9a450a5225b180fcb12dab46a2b1e7f27
Vaibhav91one <vtking03@gmail.com>
2025-04-05 12:41:03 +0530

    testing

--- components/3D/Robot.tsx
+++ components/3D/Robot.tsx
@@ -1,10 +1,12 @@
 // app/robot/page.tsx or app/page.tsx
-'use client';
 
 import Spline from "@splinetool/react-spline";
 
 export default function Robot() {
   return (
+    <div>
+      <p>Hello</p>
       <Spline scene="https://prod.spline.design/M4S11dvJnr65ej9S/scene.splinecode" />
+    </div>
     );
 }
